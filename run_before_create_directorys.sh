4a5c91e762480716a1eb0efde935dcc5643e0aa0
Daniel Bacher <{{ .email }}>
2025-06-25 10:58:30 +0200

    add ssh tmp dirs

--- run_before_create_directorys.sh
+++ run_before_create_directorys.sh
@@ -7,3 +7,11 @@ fi
 if [ ! -f ~/bin/iterm2_integration.zsh ]; then
   curl -L https://iterm2.com/shell_integration/zsh -o ~/bin/iterm2_integration.zsh
 fi
+
+if [ ! -d ~/tmp ]; then
+  mkdir -p ~/tmp
+fi
+
+if [ ! -d ~/tmp/.ssh/cm/ ]; then
+  mkdir -p ~/tmp/.ssh/cm/
+fi
